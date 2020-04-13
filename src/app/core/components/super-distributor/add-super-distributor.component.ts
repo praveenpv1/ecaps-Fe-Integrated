10e68e98b175e13647091bdc78f2af8451b5bc1e
Preshin PS <preshingithub@gmail.com>
2020-04-13 14:52:50 +0530

    corrected params

--- src/app/core/components/super-distributor/add-super-distributor.component.ts
+++ src/app/core/components/super-distributor/add-super-distributor.component.ts
@@ -3,18 +3,22 @@ import {
   FormBuilder,
   FormControl,
   FormGroup,
-  Validators
+  Validators,
 } from "@angular/forms";
-import { ADD_CHILD,GET_CHILD_USER,UPDATE_CHILD_USER } from "./../../store/actions/index";
+import {
+  ADD_CHILD,
+  GET_CHILD_USER,
+  UPDATE_CHILD_USER,
+} from "./../../store/actions/index";
 import { ActivatedRoute } from "@angular/router";
 import * as _ from "lodash";
 import * as moment from "moment";
 import { UserReducers } from "@app/core/store/reducers/user.reducer";
-import { DataStore } from "@app/core/store/app.store"; 
+import { DataStore } from "@app/core/store/app.store";
 @Component({
   selector: "app-addnewuser",
   templateUrl: "./add-super-distributor.component.html",
-  styleUrls: ["./add-super-distributor.component.scss"]
+  styleUrls: ["./add-super-distributor.component.scss"],
 })
 export class AddSuperDistributorComponent implements OnInit {
   validateForm: FormGroup;
@@ -26,7 +30,7 @@ export class AddSuperDistributorComponent implements OnInit {
     private fb: FormBuilder,
     private child: UserReducers,
     private activatedRoute: ActivatedRoute,
-    private _dataStore: DataStore, 
+    private _dataStore: DataStore
   ) {}
 
   submitForm() {
@@ -43,15 +47,17 @@ export class AddSuperDistributorComponent implements OnInit {
     }
     if (this.isFormValid) {
       if (!_.isEmpty(this._id)) {
-        if(!_.isEmpty(this.userDetails)) {
+        if (!_.isEmpty(this.userDetails)) {
           this.child.userReducer({
             type: UPDATE_CHILD_USER,
             payload: {
               id: this._id,
-              dob: moment(this.validateForm.controls.dateOfBirth.value).format("DD-MM-YYYY"),
-              is_verified: this.userDetails.is_verified,
-              status: this.userDetails.status,
-              tpin: this.userDetails.status.tpin,
+              dob: moment(this.validateForm.controls.dateOfBirth.value).format(
+                "DD-MM-YYYY"
+              ),
+              is_verified: true,
+              status: true,
+              tpin: "",
               aadhaar: this.validateForm.controls.aadhaarNo.value,
               pan: this.validateForm.controls.pan.value,
               voter_id: this.validateForm.controls.voterId.value,
@@ -63,12 +69,11 @@ export class AddSuperDistributorComponent implements OnInit {
               username: this.validateForm.controls.userName.value,
               email: this.validateForm.controls.email.value,
               role: "superdistributor",
-              updated_at: moment.utc().format()
-            }
+              updated_at: moment.utc().format(),
+            },
           });
         }
-      }
-      else {
+      } else {
         this.child.userReducer({
           type: ADD_CHILD,
           payload: {
@@ -88,15 +93,14 @@ export class AddSuperDistributorComponent implements OnInit {
             aadhaar: this.validateForm.controls.aadhaarNo.value,
             pan: this.validateForm.controls.pan.value,
             voter_id: this.validateForm.controls.voterId.value,
-            kit_number: this.validateForm.controls.kitNo.value
-          }
-        });       
+            kit_number: this.validateForm.controls.kitNo.value,
+          },
+        });
       }
     }
   }
 
   ngOnInit() {
-
     this.setForm({});
 
     if (this.activatedRoute.snapshot.paramMap.get("id")) {
@@ -104,32 +108,33 @@ export class AddSuperDistributorComponent implements OnInit {
     }
 
     if (!_.isEmpty(this._id)) {
-
-        this.child.userReducer({
+      this.child.userReducer({
         type: GET_CHILD_USER,
         payload: {
-          id: this._id
-        }
+          id: this._id,
+        },
       });
 
-    this._dataStore.dataStore$.subscribe(data => {
-      if(data.childUser) {
-        this.userDetails = data.childUser;
-        if(this.userDetails != null){
-          this.setDetails({})
-        } 
-      }
-    });
+      this._dataStore.dataStore$.subscribe((data) => {
+        if (data.childUser) {
+          this.userDetails = data.childUser;
+          if (this.userDetails != null) {
+            this.setDetails({});
+          }
+        }
+      });
     }
   }
 
-  setDetails(data: any){ 
-    if(!_.isEmpty(this.userDetails)) { 
-      this.validateForm.patchValue({ 
+  setDetails(data: any) {
+    if (!_.isEmpty(this.userDetails)) {
+      this.validateForm.patchValue({
         first_name: this.userDetails.firstname,
         last_name: this.userDetails.lastname,
         company_name: this.userDetails.company_name,
-        dateOfBirth: moment(this.userDetails.dob, "DD-MM-YYYY").format("MM-DD-YYYY"), 
+        dateOfBirth: moment(this.userDetails.dob, "DD-MM-YYYY").format(
+          "MM-DD-YYYY"
+        ),
         phoneNumber: this.userDetails.phone,
         userName: this.userDetails.username,
         email: this.userDetails.email,
@@ -140,19 +145,19 @@ export class AddSuperDistributorComponent implements OnInit {
         voterId: this.userDetails.voter_id,
         kitNo: this.userDetails.kit_number,
         // selectedMargin: [null, [Validators.required]],
-      // nonRefundableAmount: [null, [Validators.required]],
-      // securityAmount: [null, [Validators.required]],
-      // paymentMode: [null, [Validators.required]],
-      // paymentBank: [null, [Validators.required]],
-      // paymentReferenceNo:[null, [Validators.required]],
-      // paymentDate: [null],
-      // paymentRemarks: ['', [Validators.required]],
-      // kitsIssued: [null, [Validators.required]],
-      // rate: [null, [Validators.required]],
-      // complementaryKits: [null, [Validators.required]],
-      // kitsRemarks: ['', [Validators.required]]
+        // nonRefundableAmount: [null, [Validators.required]],
+        // securityAmount: [null, [Validators.required]],
+        // paymentMode: [null, [Validators.required]],
+        // paymentBank: [null, [Validators.required]],
+        // paymentReferenceNo:[null, [Validators.required]],
+        // paymentDate: [null],
+        // paymentRemarks: ['', [Validators.required]],
+        // kitsIssued: [null, [Validators.required]],
+        // rate: [null, [Validators.required]],
+        // complementaryKits: [null, [Validators.required]],
+        // kitsRemarks: ['', [Validators.required]]
       });
-    } 
+    }
   }
 
   setForm(data: any) {
@@ -160,7 +165,7 @@ export class AddSuperDistributorComponent implements OnInit {
       first_name: [null, [Validators.required]],
       last_name: [null, [Validators.required]],
       company_name: [null, [Validators.required]],
-      dateOfBirth: [null], 
+      dateOfBirth: [null],
       phoneNumber: [null, [Validators.required]],
       userName: [null, [Validators.required]],
       email: [null, [Validators.email, Validators.required]],
@@ -183,5 +188,6 @@ export class AddSuperDistributorComponent implements OnInit {
       // complementaryKits: [null, [Validators.required]],
       // kitsRemarks: ['', [Validators.required]]
     });
+    this.validateForm.controls.role.disable();
   }
 }
